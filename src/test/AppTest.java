3f200d81435ba43ff5a30d8b425cc304bebb5a13
Shunzhi Wen <swen45@gatech.edu>
2021-10-01 15:32:49 -0400

    setup the first test

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -1,2 +1,28 @@
-public class AppTest {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import controller.Controller;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+
+public class AppTest extends ApplicationTest {
+    public AppTest() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Controller controller = new Controller();
+        controller.start(primaryStage);
+    }
+
+    @Test
+    public void testPlay() {
+        this.clickOn("Start");
+        FxAssert.verifyThat("Init Config Screen", NodeMatchers.isNotNull());
+    }
 }
